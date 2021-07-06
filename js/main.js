6365de9b3f9af8c6ad24576eefe4875e1681adc0
kchelmi <hnwpol@gmail.com>
2021-07-06 11:27:21 +0200

    6.7.21 v.1

--- js/main.js
+++ js/main.js
@@ -1,124 +1,181 @@
 // GSAP Animation
-const hero = document.querySelector('.hero');
-const slider = document.querySelector('.slider');
-const logo = document.querySelector('#logo');
-const headline = document.querySelector('.headline');
-const burger = document.querySelector('.burger');
+const hero = document.querySelector(".hero");
+const slider = document.querySelector(".slider");
+const logo = document.querySelector("#logo");
+const headline = document.querySelector(".headline");
+const headline2 = document.querySelector(".headline__number__contact");
+
+const burger = document.querySelector(".burger");
 
 const tl = new TimelineMax();
 
 // tl - timeline
-tl.fromTo(hero, 1, {
-  height: '0%'
-}, {
-  height: '80%',
-  ease: Power2.easeInOut
-}).fromTo(hero, 1.2, {
-  width: '100%'
-}, {
-  width: '80%',
-  ease: Power2.easeInOut
-}).fromTo(slider, 1.2, {
-  x: '-100%'
-}, {
-  x: '0%',
-  ease: Power2.easeInOut
-}, "-=1.2").fromTo(logo, 0.5, {
-  opacity: 0,
-  x: 30
-}, {
-  opacity: 1,
-  x: 0
-}, "-=0.5").fromTo(burger, 0.5, {
-  opacity: 0,
-  x: 30
-}, {
-  opacity: 1,
-  x: 0
-}, "-=0.5").fromTo(headline, 0.5, {
-  opacity: 0,
-  x: 30
-}, {
-  opacity: 1,
-  x: 0
-}, "-=0.5")
+tl.fromTo(
+  hero,
+  1,
+  {
+    height: "0%",
+  },
+  {
+    height: "80%",
+    ease: Power2.easeInOut,
+  }
+)
+  .fromTo(
+    hero,
+    1.2,
+    {
+      width: "100%",
+    },
+    {
+      width: "80%",
+      ease: Power2.easeInOut,
+    }
+  )
+  .fromTo(
+    slider,
+    1.2,
+    {
+      x: "-100%",
+    },
+    {
+      x: "0%",
+      ease: Power2.easeInOut,
+    },
+    "-=1.2"
+  )
+  .fromTo(
+    logo,
+    0.5,
+    {
+      opacity: 0,
+      x: 30,
+    },
+    {
+      opacity: 1,
+      x: 0,
+    },
+    "-=0.5"
+  )
+  .fromTo(
+    burger,
+    0.5,
+    {
+      opacity: 0,
+      x: 30,
+    },
+    {
+      opacity: 1,
+      x: 0,
+    },
+    "-=0.5"
+  )
+  .fromTo(
+    headline,
+    0.5,
+    {
+      opacity: 0,
+      x: 30,
+    },
+    {
+      opacity: 1,
+      x: 0,
+    },
+    "-=0.5"
+  )
+  .fromTo(
+    headline2,
+    0.5,
+    {
+      opacity: 0,
+      x: 30,
+    },
+    {
+      opacity: 1,
+      x: 0,
+    },
+    "-=0.5"
+  );
 
 // Menu Animation
 
 const navSlide = () => {
-  const burger = document.querySelector('.burger');
-  const nav = document.querySelector('.nav-links');
-  const navLinks = document.querySelectorAll('.nav-links li');
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
 
-  burger.addEventListener('click', () => {
-    nav.classList.toggle('nav-active');
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
 
     navLinks.forEach((link, index) => {
       if (link.style.animation) {
-        link.style.animation = '';
+        link.style.animation = "";
       } else {
-        link.style.animation = `navLinkFade 0.4s ease forwards ${index / 7 + 0.5}s`
+        link.style.animation = `navLinkFade 0.4s ease forwards ${
+          index / 7 + 0.5
+        }s`;
       }
     });
     // Burger Animation
-    burger.classList.toggle('toggleBurger');
-    
+    burger.classList.toggle("toggleBurger");
   });
-}
+};
 
 const app = () => {
-  navSlide()
-}
+  navSlide();
+};
 
 app();
 
 // Scroll To Section jQuery
 
-$('.nav-links li a').on('click', function () {
-  const goToSection = "#" + $(this).attr('class');
+$(".nav-links li a").on("click", function () {
+  const goToSection = "#" + $(this).attr("class");
 
-  $('body, html').animate({
-    scrollTop: $(goToSection).offset().top
-  }, 1000)
-  nav.classList.remove('nav-active')
-  burger.classList.toggle('toggleBurger');
-})
+  $("body, html").animate(
+    {
+      scrollTop: $(goToSection).offset().top,
+    },
+    1000
+  );
+  nav.classList.remove("nav-active");
+  burger.classList.toggle("toggleBurger");
+});
 
-// Animation fadeIn 
+// Animation fadeIn
 
-const fadeIns = document.querySelectorAll('.fadeIn')
+const fadeIns = document.querySelectorAll(".fadeIn");
 
-window.addEventListener('scroll', checkfadeIn);
+window.addEventListener("scroll", checkfadeIn);
 
-checkfadeIn()
+checkfadeIn();
 
 function checkfadeIn() {
-  const triggerfadeIn = window.innerHeight / 5 * 4;
+  const triggerfadeIn = (window.innerHeight / 5) * 4;
 
-  fadeIns.forEach(fadeIn => {
-    const fadeInTop = fadeIn.getBoundingClientRect().top
+  fadeIns.forEach((fadeIn) => {
+    const fadeInTop = fadeIn.getBoundingClientRect().top;
 
     if (fadeInTop < triggerfadeIn) {
-      fadeIn.classList.add('animate-fadeIn')
+      fadeIn.classList.add("animate-fadeIn");
     }
-  })
+  });
 }
 
+const zoomIns = document.querySelectorAll(".zoomIn");
 
-const zoomIns = document.querySelectorAll('.zoomIn')
+window.addEventListener("scroll", checkzoomIn);
 
-window.addEventListener('scroll', checkzoomIn);
-
-checkzoomIn()
+checkzoomIn();
 
 function checkzoomIn() {
-  const triggerzoomIn = window.innerHeight / 5 * 4;
+  const triggerzoomIn = (window.innerHeight / 5) * 4;
 
-  zoomIns.forEach(zoomIn => {
-    const zoomInTop = zoomIn.getBoundingClientRect().top
+  zoomIns.forEach((zoomIn) => {
+    const zoomInTop = zoomIn.getBoundingClientRect().top;
 
     if (zoomInTop < triggerzoomIn) {
-      zoomIn.classList.add('animate-zoomIn')
+      zoomIn.classList.add("animate-zoomIn");
     }
-  })
+  });
 }
